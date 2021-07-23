f8c99c08edae655896ee93ce4d22d2fe757fa7be
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-23 17:37:45 -0400

    Added aos, css & js animation to the site

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,51 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+	// Navbar shrink function
+	var navbarShrink = function () {
+		const navbarCollapsible = document.body.querySelector('#mainNav');
+		if (!navbarCollapsible) {
+			return;
+		}
+		if (window.scrollY === 0) {
+			navbarCollapsible.classList.remove('navbar-shrink');
+		} else {
+			navbarCollapsible.classList.add('navbar-shrink');
+		}
+	};
 
+	// Shrink the navbar
+	navbarShrink();
 
-window.addEventListener('DOMContentLoaded', event => {
+	// Shrink the navbar when page is scrolled
+	document.addEventListener('scroll', navbarShrink);
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+	// Activate Bootstrap scrollspy on the main nav element
+	const mainNav = document.body.querySelector('#mainNav');
+	if (mainNav) {
+		new bootstrap.ScrollSpy(document.body, {
+			target: '#mainNav',
+			offset: 74,
+		});
+	}
 
-    };
+	// Collapse responsive navbar when toggler is visible
+	const navbarToggler = document.body.querySelector('.navbar-toggler');
+	const responsiveNavItems = [].slice.call(
+		document.querySelectorAll('#navbarResponsive .nav-link')
+	);
+	responsiveNavItems.map(function (responsiveNavItem) {
+		responsiveNavItem.addEventListener('click', () => {
+			if (window.getComputedStyle(navbarToggler).display !== 'none') {
+				navbarToggler.click();
+			}
+		});
+	});
+});
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
+window.addEventListener('load', () => {
+	AOS.init({
+		duration: 1000,
+		easing: 'ease-in-out',
+		once: true,
+		mirror: false,
+	});
 });
