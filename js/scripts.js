fa55847343831d6ab6573be16afa130031f2c45f
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-07-30 18:08:42 -0400

    Added Jquery to help with toggle style fixed featured projects changes font

--- js/scripts.js
+++ js/scripts.js
@@ -49,3 +49,19 @@ window.addEventListener('load', () => {
 		mirror: false,
 	});
 });
+
+// Featured projects button
+// var button = document.getElementById('button');
+
+// button.onclick = function () {
+// 	var div = document.getElementById('newpost');
+// 	if (div.style.display !== 'none') {
+// 		div.style.display = 'none';
+// 	} else {
+// 		div.style.display = 'block';
+// 	}
+// };
+
+$('#button').click(function () {
+	$('#newpost').slideToggle();
+});
